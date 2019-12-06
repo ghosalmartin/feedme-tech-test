0706ecb21ee11dd80fe70e4fb40bdd15b1a3abd0
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2019-12-06 10:10:24 +0000

    Pass the data parsed by the consumer to the producer

--- src/main/kotlin/com/cba/feedmetechtest/demo/DemoApplication.kt
+++ src/main/kotlin/com/cba/feedmetechtest/demo/DemoApplication.kt
@@ -1,11 +1,20 @@
 package com.cba.feedmetechtest.demo
 
+import com.cba.feedmetechtest.demo.messaging.Consumer
+import com.cba.feedmetechtest.demo.messaging.Producer
+import com.cba.feedmetechtest.demo.models.toMessage
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
 
+private val consumer = Consumer()
+private val producer = Producer()
+
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
+    consumer.consume {
+        producer.produce(it.toMessage())
+    }
 }
