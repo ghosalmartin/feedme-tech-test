91f1bf9a5dcc7ed8c77c63ce543bd2665e3e0dfe
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2019-12-06 10:02:56 +0000

    Create the producer/consumer objects

---
+++ src/main/kotlin/com/cba/feedmetechtest/demo/messaging/Consumer.kt
@@ -0,0 +1,16 @@
+package com.cba.feedmetechtest.demo.messaging
+
+import java.io.BufferedReader
+import java.net.Socket
+import javax.net.SocketFactory
+
+class Consumer(
+    socket: Socket = SocketFactory.getDefault().createSocket("localhost", 8282),
+    private val bufferedReader: BufferedReader = socket.getInputStream().bufferedReader()
+) {
+    fun consume(listener: (String) -> Unit){
+        bufferedReader.useLines { sequence ->
+            sequence.iterator().forEach { listener.invoke(it) }
+        }
+    }
+}
---
+++ src/main/kotlin/com/cba/feedmetechtest/demo/messaging/Producer.kt
@@ -0,0 +1,9 @@
+package com.cba.feedmetechtest.demo.messaging
+
+import com.cba.feedmetechtest.demo.models.Message
+
+class Producer {
+    fun produce(message: Message) {
+
+    }
+}
